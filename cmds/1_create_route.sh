6375a356da40d51826481743d80aa54f973c4b0c
Nicolas Fränkel <nicolas@frankel.ch>
2024-04-25 09:10:14 +0200

    Update 1_create_route.sh

--- cmds/1_create_route.sh
+++ cmds/1_create_route.sh
@@ -1,7 +1,7 @@
-curl "http://127.0.0.1:9180/apisix/admin/routes/1" -H "X-API-KEY: edd1c9f034335f136f87ad84b625c8f1" -X PUT -d '  <1><2><3>
+curl http://localhost:9180/apisix/admin/routes/1 -H "X-API-KEY: edd1c9f034335f136f87ad84b625c8f1" -X PUT -d '  <1><2><3>
 {
-  "uri": "/anything*",                    <6>
-  "upstream": {                           <7>
+  "uri": "/anything*",                    <4>
+  "upstream": {                           <5>
     "nodes": {
       "httpbin:80": 1
     }
