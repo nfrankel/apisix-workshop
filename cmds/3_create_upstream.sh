82fb3928729565c815b189b82d7bbe0b9ae11dba
Nicolas Fränkel <nicolas@frankel.ch>
2024-04-25 09:21:10 +0200

    Update 3_create_upstream.sh

--- cmds/3_create_upstream.sh
+++ cmds/3_create_upstream.sh
@@ -1,7 +1,6 @@
-curl "http://127.0.0.1:9080/apisix/admin/upstreams/1" -H "X-API-KEY: edd1c9f034335f136f87ad84b625c8f1" -X PUT -d '
+curl http://localhost:9180/apisix/admin/upstreams/1 -H "X-API-KEY: edd1c9f034335f136f87ad84b625c8f1" -X PUT -d '
 {
-  "type": "roundrobin",
   "nodes": {
-    "httpbin.org:80": 1
+    "httpbin:80": 1
   }
 }'
